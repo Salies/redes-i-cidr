cf724dafc10d31043b814d49bf0708bfd66c04ab
Salies <serezane@protonmail.com>
2022-08-23 20:15:11 -0300

    0 permitido e 32 fix

--- main.c
+++ main.c
@@ -8,12 +8,13 @@
 #include <stdint.h>
 
 // Protótipos
-uint32_t p(unsigned char n);
+unsigned long long p(unsigned char n);
 
+// ulonglong não seria necessário, mas sinceramente não quero fazer tratamento especial...
 // Retorna 2 elevado a n
-uint32_t p(unsigned char n) {
+unsigned long long p(unsigned char n) {
     if(n == 0) return 1; // não deve acontecer, mas...
-    uint32_t out = 2;
+    unsigned long long out = 2;
     for(unsigned char i = 1; i < n; i++)
         out *= 2;
     return out;
@@ -34,10 +35,10 @@ int main(void) {
     // Pegando uma notação de máscara (número de 1-32) válida.
     // uchar é o menor número possível de alocar (1 byte) pela linguagem C
     unsigned char mask_p;
-    printf("Escolha uma máscara (1-32): /");
+    printf("Escolha uma máscara (0-32): /");
     scanf("%hhu", &mask_p);
 
-    if(mask_p < 1 || mask_p > 32) {
+    if(mask_p < 0 || mask_p > 32) {
         printf("Máscara inválida.\n");
         return 1;
     }
@@ -61,7 +62,7 @@ int main(void) {
     unsigned char* broad_oct = (unsigned char*) & broad;
 
     // Quantidade de hosts
-    uint32_t h = p(32 - mask_p);
+    unsigned long long h = p(32 - mask_p);
 
     // Exibe o resultado
     printf("\nEndereço IP: %hhu.%hhu.%hhu.%hhu/%d", ip_oct[3], ip_oct[2], ip_oct[1], ip_oct[0], mask_p);
@@ -69,9 +70,13 @@ int main(void) {
     printf("\nMáscara wildcard: %hhu.%hhu.%hhu.%hhu", wild_oct[3], wild_oct[2], wild_oct[1], wild_oct[0]);
     printf("\nEndereço de rede: %hhu.%hhu.%hhu.%hhu", net_oct[3], net_oct[2], net_oct[1], net_oct[0]);
     printf("\nEndereço de broadcast: %hhu.%hhu.%hhu.%hhu", broad_oct[3], broad_oct[2], broad_oct[1], broad_oct[0]);
-    printf("\nNúmero de hosts: %d", h);
-    printf("\nNuméro de hosts válidos: %d", h - 2);
-    printf("\nRange usável de endereços: %hhu.%hhu.%hhu.%hhu - %hhu.%hhu.%hhu.%hhu\n", 
+    printf("\nNúmero de endereços: %llu", h);
+    if (h >= 2)
+        printf("\nNuméro de endereços válidos: %llu", h - 2);
+    if(h <= 2)
+        printf("\nNão há range endereços usáveis.\n");
+    else
+        printf("\nRange usável de endereços: %hhu.%hhu.%hhu.%hhu - %hhu.%hhu.%hhu.%hhu\n", 
     net_oct[3], net_oct[2], net_oct[1], (net_oct[0] + 1), 
     broad_oct[3], broad_oct[2], broad_oct[1], (broad_oct[0] - 1));
 
